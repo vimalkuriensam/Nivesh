83a90d2b13e9c7b9fbdef203a963b5e82b08f5b7
unknown <vimalkuriensam@gmail.com>
2022-02-04 14:50:58 +0530

    changed external mappings

--- core/externalMapping.js
+++ core/externalMapping.js
@@ -1,24 +1,24 @@
 const externalMapping = {
-  "/webapp/": "https://app.nivesh.com",
+  "/webapp/*": "https://app.nivesh.com",
   "/webapp": "https://app.nivesh.com",
-  "/gyan/all-you-should-know-about-liquid-funds":
+  "/gyan/all-you-should-know-about-liquid-funds/*":
     "https://blog.nivesh.com/mf/debt/all-you-should-know-about-liquid-funds/",
-  "/gyan/fmps-a-perfect-choice-for-investors-looking-for-steady-returns":
+  "/gyan/fmps-a-perfect-choice-for-investors-looking-for-steady-returns/*":
     "https://blog.nivesh.com/",
-  "/gyan/take-baby-steps-towards-investment-with-ultra-short-term-funds":
+  "/gyan/take-baby-steps-towards-investment-with-ultra-short-term-funds/":
     "https://blog.nivesh.com/mf/debt/what-are-ultra-short-term-funds",
   "/crm": "https://crm.nivesh.com",
   "/Providential_CMS": "https://cms.nivesh.com",
   "/webapi": "https://api.nivesh.com",
-  "/webapi/": "https://api.nivesh.com",
+  "/webapi/*": "https://api.nivesh.com",
   "/gyan/all-you-should-know-about-liquid-funds":
     "https://blog.nivesh.com/mf/debt/all-you-should-know-about-liquid-funds/",
-  "/gyan/fmps-a-perfect-choice-for-investors-looking-for-steady-returns/":
+  "/gyan/fmps-a-perfect-choice-for-investors-looking-for-steady-returns/*":
     "https://blog.nivesh.com/",
-  "/gyan/take-baby-steps-towards-investment-with-ultra-short-term-funds/":
+  "/gyan/take-baby-steps-towards-investment-with-ultra-short-term-funds/*":
     "https://blog.nivesh.com/mf/debt/what-are-ultra-short-term-funds",
-  "/crm/": "https://crm.nivesh.com",
-  "/Providential_CMS/": "https://cms.nivesh.com",
+  "/crm/*": "https://crm.nivesh.com",
+  "/Providential_CMS/*": "https://cms.nivesh.com",
 };
 
 module.exports = { externalMapping };
